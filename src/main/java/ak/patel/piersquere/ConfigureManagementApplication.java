2c5a1c9e2a686ed96ec1afedd269ac6b92d050df
techsivanag <45100885+techsivanag@users.noreply.github.com>
2024-06-26 14:53:52 +0530

    Add files via upload

---
+++ src/main/java/ak/patel/piersquere/ConfigureManagementApplication.java
@@ -0,0 +1,15 @@
+package ak.patel.piersquere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigureManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigureManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ak/patel/piersquere/ConfigureManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ak.patel.piersquere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigureManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
